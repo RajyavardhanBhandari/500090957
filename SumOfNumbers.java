67d74c38f8ff93a2eacf62ba1cebd41e57449de0
RajyavardhanBhandari <rajb822@gmail.com>
2022-09-21 17:27:43 +0530

    Adding Java file

---
+++ SumOfNumbers.java
@@ -0,0 +1,56 @@
+// //Experiment 2&3_2   27-08-2022
+//Author: Rajyavardhan Bhandari 
+
+import java.util.Scanner;  
+
+class SumOfNumbers{
+    public static void main(String[] args) {
+        int a,b,sum;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number");
+        a = sc.nextInt();
+
+        System.out.println("Enter second number");
+        b = sc.nextInt();
+
+       
+        //addinf
+        sum= a + b;
+            
+        System.out.println("The sum numbers is: " +sum);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// class SumOfNumbers{
+//     public static void main(String[] args) {
+//         int a,b,sum;
+
+       
+
+//         System.out.println("Enter first number");
+//         a = Integer.parseInt(args[0]);
+
+//         System.out.println("Enter second number");
+//         b = Integer.parseInt(args[1]);
+
+       
+//         //addinf
+//         sum= a + b;
+            
+//         System.out.println("The sum numbers is: " +sum);
+//     }
+
+// }
